631bb7709ce79dbf8a92a7bff49edbcbaca0a9a1
kerabite <10791585+kerabite@users.noreply.github.com>
2023-11-09 17:35:29 -0600

    Implemented QuitCommand

--- src/QuitCommand.java
+++ src/QuitCommand.java
@@ -2,8 +2,7 @@ public class QuitCommand implements Command {
 
   @Override
   public void run(BlockChainDriver instance) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'run'");
+    instance.isRunning = false;
   }
   
 }
