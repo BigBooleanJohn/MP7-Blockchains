a60ba550e59ea109391d7ad42c5a5c53b140538f
kerabite <10791585+kerabite@users.noreply.github.com>
2023-11-09 16:38:37 -0600

    Made unit tests for Hash

---
+++ test/HashTest.java
@@ -0,0 +1,39 @@
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+public class HashTest {
+  @Test
+  public void isEqualTest() {
+    try {
+      Hash hash1, hash2;
+      hash1 = new Hash(new byte[] {0, 1, 2, 3, 4});
+      hash2 = new Hash(new byte[] {0, 1, 2, 3, 4});
+      assertTrue(hash1.equals(hash2));
+    } catch (Exception e) {
+      fail();
+    }
+  }
+
+  @Test
+  public void calculateHashTest() {
+    try {
+      Hash hash1, hash2;
+      hash1 = new Hash(Hash.calculateHash(new byte[] {0, 1, 2, 3, 4}));
+      hash2 = new Hash(Hash.calculateHash(new byte[] {0, 1, 2, 3, 4}));
+      assertTrue(hash1.equals(hash2));
+    } catch (Exception e) {
+      fail();
+    }
+  }
+
+  @Test
+  public void isValidTest() {
+    try {
+      Hash hash = new Hash(new byte[] {0, 0, 0, 1});
+      assertTrue(hash.isValid());
+    } catch (Exception e) {
+      fail();
+    }
+  }
+}
