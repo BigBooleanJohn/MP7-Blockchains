3322bb00b8239c9fa71b552f577ac56776ad0728
kerabite <10791585+kerabite@users.noreply.github.com>
2023-11-09 17:55:24 -0600

    Altered BlockTest

--- test/BlockTest.java
+++ test/BlockTest.java
@@ -7,9 +7,9 @@ public class BlockTest {
   @Test
   public void firstBlockCreationTest() {
     try {
-      Block testBlock = new Block(1, 2, null);
-      assertEquals(1, testBlock.getNum());
-      assertEquals(2, testBlock.getAmount());
+      Block testBlock = new Block(0, 1, null);
+      assertEquals(0, testBlock.getNum());
+      assertEquals(1, testBlock.getAmount());
       assertTrue(testBlock.getHash().isValid());
       assertTrue(testBlock.getNonce() >= 0);
     } catch (Exception e) {
@@ -20,10 +20,10 @@ public class BlockTest {
   @Test
   public void blockCreationTest() {
     try {
-      Block firstBlock = new Block(1, 2, null);
-      Block testBlock = new Block(2, 1, firstBlock.getHash());
-      assertEquals(2, testBlock.getNum());
-      assertEquals(1, testBlock.getAmount());
+      Block firstBlock = new Block(0, 1, null);
+      Block testBlock = new Block(1, 2, firstBlock.getHash());
+      assertEquals(1, testBlock.getNum());
+      assertEquals(2, testBlock.getAmount());
       assertTrue(testBlock.getHash().isValid());
       assertTrue(testBlock.getPrevHash().isValid());
       assertTrue(testBlock.getNonce() >= 0);
