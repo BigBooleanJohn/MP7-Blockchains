74ec20833f5e1b14f6726d79b8b10fa4898234d7
kerabite <10791585+kerabite@users.noreply.github.com>
2023-11-09 20:35:34 -0600

    Added Command documentation

--- src/driver/commands/Command.java
+++ src/driver/commands/Command.java
@@ -2,7 +2,13 @@ package driver.commands;
 import driver.BlockChainDriver;
 
 public interface Command {
+  /**
+   * Runs the command on the current BlockChainDriver instance
+   */
   public void run(BlockChainDriver instance);
 
+  /**
+   * Returns the description of this command.
+   */
   public String getDescription();
 }
