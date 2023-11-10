ded477aa658157108d444a21bdcf1883bde713a7
kerabite <10791585+kerabite@users.noreply.github.com>
2023-11-09 19:03:24 -0600

    Implement RemoveCommand

--- src/driver/commands/RemoveCommand.java
+++ src/driver/commands/RemoveCommand.java
@@ -5,8 +5,7 @@ public class RemoveCommand implements Command {
 
   @Override
   public void run(BlockChainDriver instance) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'run'");
+    instance.getBlockChain().removeLast();
   }
 
   @Override
