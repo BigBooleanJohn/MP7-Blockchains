52c9bd652ed5f6d264168f25fd4f6b857f8fd110
kerabite <10791585+kerabite@users.noreply.github.com>
2023-11-09 18:47:03 -0600

    Implement MineCommand

--- src/driver/commands/MineCommand.java
+++ src/driver/commands/MineCommand.java
@@ -1,12 +1,30 @@
 package driver.commands;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import blockchain.BlockChain;
 import driver.BlockChainDriver;
 
 public class MineCommand implements Command {
 
   @Override
   public void run(BlockChainDriver instance) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'run'");
+    PrintWriter pen = instance.getPen();
+    BufferedReader input = instance.getInputReader();
+
+    pen.print("Amount transferred? ");
+    pen.flush();
+
+    int amount;
+
+    try {
+      amount = Integer.parseInt(input.readLine());
+    } catch (Exception e) {
+      pen.println("ERROR: Invalid amount given!");
+      return;
+    }
+
+    BlockChain blockChain = instance.getBlockChain();
+    blockChain.mine(amount);
   }
 
   @Override
